8c934f74b6d0f706d38997d5d94e62025b054f03
OLGBollner <oskar.bollner@gmail.com>
2026-09-03 17:55:00 +0200

    Added to parsers

--- scripts/package_raw_data.py
+++ scripts/package_raw_data.py
@@ -3,8 +3,9 @@ from beyblade.parsers import parse_zfs_simulation_dataset
 
 def main():
   data_path = sys.argv[1]
+  method = str(sys.argv[2])
   print(f"Collecting data from {data_path}")
-  raw_data = parse_zfs_simulation_dataset(data_path)
+  raw_data = parse_zfs_simulation_dataset(data_path, method=method)
 
   save_path = raw_data.save()
 
